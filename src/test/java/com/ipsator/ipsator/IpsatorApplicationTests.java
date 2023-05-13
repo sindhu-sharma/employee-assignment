c2892bfc9860f4b82bc00fd5a119d9fbce77b846
sindhu-sharma <sindhusharma2297@gmail.com>
2023-05-13 13:22:00 +0530

    first commit

---
+++ src/test/java/com/ipsator/ipsator/IpsatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipsator.ipsator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpsatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
